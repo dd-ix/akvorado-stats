be2464431d969bf77709cefa456b32c2c71cc81f
Thomas Liske <thomas@fiasko-nw.net>
2024-07-19 00:08:51 +0200

    add save and upload actions

--- akvo-top-asn.conf.ex
+++ akvo-top-asn.conf.ex
@@ -8,5 +8,11 @@
 # host = localhost
 
 [upload]
-# url = https://
-# filename_pattern = 
+## upload server & auth (i.e. Nextcloud public share)
+# url = https://cloud.dd-ix.net/public.php/webdav/
+# username = nc-share-id
+# password = nc-share-secret
+
+## filename pattern (strftime), we be appended to the
+## upload url and used for saving to local files
+# filename = export-%Y-W%W.yml
