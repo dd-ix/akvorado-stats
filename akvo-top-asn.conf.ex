20ff7fae254f8cb0ddb95eec24944ae52e7198f9
tanneberger <revol-xut@protonmail.com>
2024-07-19 20:18:48 +0200

    quering with limit and offset

--- akvo-top-asn.conf.ex
+++ akvo-top-asn.conf.ex
@@ -7,6 +7,17 @@
 ## Use localhost:8123 via http (default):
 # host = localhost
 
+[query]
+## How to query clickhouse
+
+## how many rows per query
+# query_limit = 100
+
+## how many rows will be extracted in total
+# value_limit = 1000
+
+## the maximum number of queries against clickhouse is value_limit / query_limit
+
 [upload]
 ## upload server & auth (i.e. Nextcloud public share)
 # url = https://cloud.dd-ix.net/public.php/webdav
